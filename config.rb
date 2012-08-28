742f518bbed7519df96dd4bce10e0cee138514ad
stevector <steve@stevepersch.com>
2012-08-28 00:08:18 -0500

    Starting on Zen Grids.

--- config.rb
+++ config.rb
@@ -6,6 +6,7 @@ css_dir = "theme/css"
 sass_dir = "theme/scss"
 images_dir = "images"
 javascripts_dir = "js"
+require 'zen-grids'
 
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :compressed #:expanded or :nested or :compact or :compressed
--- slide_config.js
+++ slide_config.js
@@ -1,14 +1,14 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: 'Zen Grids: <br>A brief introduction',
+    subtitle: 'Chicago Advanced Drupal User Group. Aug 28th, 2012',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/favicon.ico',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Inconsolata'
@@ -18,12 +18,12 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Steve Persch',
+    company: 'Engineer, Palantir',
+//    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@stevector',
+//    www: 'http://www.you.com',
+    github: 'https://github.com/stevector'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
